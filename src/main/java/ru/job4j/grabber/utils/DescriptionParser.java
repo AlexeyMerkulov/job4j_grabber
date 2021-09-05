16c8b87e1c623d4d581f5e46cfaa5cd030b215b0
AlexeyMerkulov <alexeymerkulovv@gmail.com>
2021-09-05 20:38:30 +0300

    2.3. Загрузка деталей поста. [#285212]

---
+++ src/main/java/ru/job4j/grabber/utils/DescriptionParser.java
@@ -0,0 +1,32 @@
+package ru.job4j.grabber.utils;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class DescriptionParser {
+
+    private String parseInfo(String link, String marker, int elementIndex) {
+        String info = null;
+        try {
+            Document doc = Jsoup.connect(link).get();
+            Elements row = doc.select(marker);
+            Element desc = row.get(elementIndex);
+            info = desc.text();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return info;
+    }
+
+    public String getDescription(String link) {
+        return parseInfo(link, ".msgBody", 1);
+    }
+
+    public String getCreatedData(String link) {
+        String data = parseInfo(link, ".msgFooter", 0);
+        String[] dataArray = data.split(" \\[");
+        return dataArray[0];
+    }
+}
