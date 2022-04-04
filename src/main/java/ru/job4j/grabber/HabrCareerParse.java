47cff10a14daf947ddd8ccd168cffa0d99b9e5ff
AlexeyMerkulov <alexeymerkulovv@gmail.com>
2022-04-04 22:42:08 +0300

    2.4. SqlRuParse [#285213 #201225]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -4,19 +4,63 @@ import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.DateTimeParser;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-public class HabrCareerParse {
+public class HabrCareerParse implements Parse {
 
-    private String retrieveDescription(String link) throws IOException {
+    private final DateTimeParser dateTimeParser;
+
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
+
+    @Override
+    public List<Post> list(String link) {
+        List<Post> list = new ArrayList<>();
+        try {
+            Connection connection = Jsoup.connect(link);
+            Document document = connection.get();
+            Elements rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> {
+                Element dateElement = row.select(".vacancy-card__date").first();
+                Element titleElement = row.select(".vacancy-card__title").first();
+                Element linkElement = titleElement.child(0);
+                String date = dateElement.child(0).attr("datetime");
+                String vacancyName = titleElement.text();
+                String vacancyLink = String.format("%s%s", "https://career.habr.com", linkElement.attr("href"));
+                Post post = getPost(vacancyName, vacancyLink, date);
+                list.add(post);
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    private Post getPost(String title, String link, String date) {
+        String description = retrieveDescription(link);
+        LocalDateTime dateTime = dateTimeParser.parse(date);
+        return new Post(title, link, description, dateTime);
+    }
+
+    private String retrieveDescription(String link) {
         StringBuilder sb = new StringBuilder();
-        Connection connection = Jsoup.connect(link);
-        Document document = connection.get();
-        Element description = document.selectFirst(".style-ugc");
-        for (Element el : description.children()) {
-             sb.append(el.text());
-             sb.append(System.lineSeparator());
+        try {
+            Connection connection = Jsoup.connect(link);
+            Document document = connection.get();
+            Element description = document.selectFirst(".style-ugc");
+            for (Element el : description.children()) {
+                sb.append(el.text());
+                sb.append(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return sb.toString();
     }
